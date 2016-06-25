4ad51bb82f68026f7042235f895ce5401d7d4c45
maryu <beskey4646@gmail.com>
2016-06-25 12:04:07 +0000

    posts_controller create index.html create

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,34 @@
+class PostsController < ApplicationController
+
+	def index 
+		
+	end
+
+	def show
+		
+	end
+
+	def new
+		
+	end
+
+	def edit
+		
+	end
+
+	def create
+
+	end
+
+	def update
+		
+	end
+		
+
+	def destroy
+		
+	end
+
+end
+
+

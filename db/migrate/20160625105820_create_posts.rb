c006fcbf48131acdfa0bde7186c93c9cd0214cb4
maryu <beskey4646@gmail.com>
2016-06-25 10:59:30 +0000

    create model in post

---
+++ db/migrate/20160625105820_create_posts.rb
@@ -0,0 +1,13 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.integer :user_id
+      t.string :title
+      t.text :body
+      t.string :image
+      t.integer :category_id
+
+      t.timestamps null: false
+    end
+  end
+end

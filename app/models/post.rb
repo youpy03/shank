a5e2ac1ed2043d1f1bbce29cd8dac97c27bc6164
youpy <youpy_yagi0311@yahoo.co.jp>
2016-07-02 12:37:14 +0000

    fix various funcition

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
-	has_many :likes, dependent: :destroy
-def liked_by? user
-    likes.where(user_id: user.id).exists?
-end
+	has_many :comments
+	has_many :likes
+	def liked_by? user
+	    likes.where(user_id: user.id).exists?
+	end
 end

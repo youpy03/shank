7ec1402bad83a4ae974948ab82edc38f27ce3620
youpy <youpy_yagi0311@yahoo.co.jp>
2016-07-02 11:51:04 +0000

    add comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+
   get 'users/index'
   get 'users/show'
 
   devise_for :users
-  resources :posts
+  resources :posts, shallow: true do
+#sなし = index なし
+  resource :comments, only: [:create,:destroy]
+  end
   root 'posts#index'
+
 
     # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/migrate/20160625105820_create_posts.rb
+++ db/migrate/20160625105820_create_posts.rb
@@ -6,7 +6,7 @@ class CreatePosts < ActiveRecord::Migration
       t.text :body
       t.string :image
       t.integer :category_id
-
+      
       t.timestamps null: false
     end
   end

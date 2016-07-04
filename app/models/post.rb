9625a24c879b77376e68358f13f1d333bc231022
youpy <youpy_yagi0311@yahoo.co.jp>
2016-07-04 13:24:41 +0000

    update 0704

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,9 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
 	has_many :likes
+	has_many :categorys
 	def liked_by? user
 	    likes.where(user_id: user.id).exists?
 	end
 end
+
